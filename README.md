39704a8abbe25f029d32f7a2dcf152a9d41548c4
LiangShuailong <zju_shuailong@163.com>
2014-10-06 00:49:37 +0800

    add function and bugfix

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 #MusicGarden
 
+##Author
+Liang Shuailong(@handsome)
+
+zju_shuailong@163.com
+
 ##TODO
-1. 类别以及简介的自动生成
-2. 异常处理（数据缺失、404错误等）
-3. 相关推荐的实现
-4. 修复Footer缺失的BUG
-5. HTML5 feature
-6. 其他格式图片的自动PNG转换
+[Done] 1. 类别以及简介的自动生成
+[Done] 2. 异常处理（数据缺失、404错误等?）
+[Done] 3. 修复Footer缺失的BUG
+
+4. 优化代码结构
+5. 相关推荐的实现
+6. HTML5 feature
+7. 其他格式图片的自动PNG转换
+8. END字符去掉
+9. 色块颜色随机化
--- start.js
+++ start.js
@@ -3,7 +3,8 @@ var url = require("url");
 var fs = require("fs");
 var qs = require("querystring");
 
-var PREFIX = "/Projects/Github/MusicGarden"
+var PREFIX = "/Projects/Github/MusicGarden";
+
 function start(route) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
@@ -24,17 +25,51 @@ function start(route) {
       response.writeHead(200, {
         "Content-Type": "text/html"
       });
-      if (pathname == PREFIX + '/category.html') {
+      if(pathname == PREFIX + '/index.html'){
+        // index
+        pathname = decodeURIComponent(pathname);
+        var data = fs.readFileSync(pathname, 'UTF-8');
+        var cates = fs.readdirSync(PREFIX + '/data/songs/');
+        var images = fs.readdirSync(PREFIX + '/images/');
+        for(var i = 0; i < cates.length ;++i){
+          var tuple;
+          
+          var j;
+          for(j = 0; j < images.length; ++j){
+            if(images[j] == cates[i] + '.jpg'){
+              break;
+            }
+          }
+          if(j == images.length){
+            //tuple = '<section class="6u"><a href="category.html?categoryid=' 
+            //+ i 
+            //+ '" class="image-index full"><img src="data/meta/cate_cover.png" alt="CATE_NAME"></a><header><h2>CATE_NAME</h2></header><p></p></section>';
+            tuple = '<section class="6u"><a href="category.html?categoryid=' 
+            + i 
+            + '" class="image-index full"><div class="cate_cover">CATE_NAME</div></a><header><h2>CATE_NAME</h2></header><p></p></section>';
+          }
+          else{
+            tuple = '<section class="6u"><a href="category.html?categoryid=' 
+            + i 
+            + '" class="image-index full"><img src="images/CATE_NAME.jpg" alt="CATE_NAME"></a><header><h2>CATE_NAME</h2></header><p></p></section>';
+          }
+
+          tuple = tuple.replace(/CATE_NAME/g, cates[i]);
+          var arr = data.split('END');
+          data = arr[0] + tuple + 'END' + arr[1];
+        }
+
+        response.write(data);
+        response.end();
+      }
+      else if (pathname == PREFIX + '/category.html') {
         var catid = qs.parse(query)["categoryid"];
         var cate_name = '';
         var cate_folder = '';
-        if(catid == 1){
-          cate_name = '金木水火土';
-        }
-        else if(catid ==2){
-          cate_name = '遇见一个人的星光';
-        }
-        cate_folder = PREFIX + '/data/' + cate_name + '/';
+        var cates = fs.readdirSync(PREFIX + '/data/songs/');
+        cate_name = cates[catid];
+
+        cate_folder = PREFIX + '/data/songs/' + cate_name + '/';
         files = fs.readdirSync(cate_folder);
       
         var songs = [];
@@ -55,12 +90,31 @@ function start(route) {
         var data = fs.readFileSync(pathname, 'UTF-8');
         var res = data;
         
-        for (var i = 2; i < songs.length; ++i) {
-          var tuple = '<section class="2u"><a href="song.html?categoryid=' 
-           + catid + '&songid=' + i + '" class="image full"><img src="data/'
-           + cate_name 
-           + '/songname.png" alt=""></a><header><h4>songname</h4></header></section>';
-          tuple = tuple.replace(/songname/g, songs[i]);
+        res = res.replace(/CATE_NAME/g, cate_name);
+        var count =  songs.length;
+           
+        res = res.replace(/SONGS_NUM/g, count);
+        for (var i = 0; i < songs.length; ++i) {
+          var tuple;
+          var j;
+          for(j = 0; j < files.length; ++j){
+            if(files[j] == songs[i] + '.png'){
+              break;
+            }
+          }
+          if(j == files.length){
+            //tuple = '<section class="2u"><a href="song.html?categoryid=' 
+            // + catid + '&songid=' + i + '" class="image full"><img src="data/meta/cover.png" alt="SONGNAME"></a><header><h4>SONGNAME</h4></header></section>';
+             tuple = '<section class="2u"><a href="song.html?categoryid=' 
+             + catid + '&songid=' + i + '" class="image full"><div class="song_cover">SONGNAME</div></a><header><h4>SONGNAME</h4></header></section>';
+          }
+          else{
+            tuple = '<section class="2u"><a href="song.html?categoryid=' 
+             + catid + '&songid=' + i + '" class="image full"><img src="data/songs/'
+             + cate_name 
+             + '/SONGNAME.png" alt="SONGNAME"></a><header><h4>SONGNAME</h4></header></section>';
+          }
+          tuple = tuple.replace(/SONGNAME/g, songs[i]);
 
           var arr = res.split("END");
           res = arr[0] + tuple + "END" + arr[1];
@@ -74,13 +128,11 @@ function start(route) {
         var catid = qs.parse(query)["categoryid"];
         var cate_name = '';
         var cate_folder = "";
-        if(catid == 1){
-          cate_name = '金木水火土';
-        }
-        else if(catid ==2){
-          cate_name = '遇见一个人的星光';
-        }
-        cate_folder = PREFIX + '/data/' + cate_name + '/';
+
+        var cates = fs.readdirSync(PREFIX + '/data/songs/');
+        cate_name = cates[catid];
+
+        cate_folder = PREFIX + '/data/songs/' + cate_name + '/';
         files = fs.readdirSync(cate_folder);
         var songs = [];
         for (var i = 0; i < files.length; ++i) {
@@ -104,9 +156,29 @@ function start(route) {
         });
         pathname = decodeURIComponent(pathname);
         var data = fs.readFileSync(pathname, 'UTF-8');
+
+        var i;
+          for(i = 0; i < files.length; ++i){
+            if(files[i] == songs[song_id] + '.png'){
+              break;
+            }
+          }
+          if(i == files.length){
+            data = data.replace(/songs\/CATEGORY\/SONG_COVER/g, 'meta/cover');
+          }
+          else{
+            data = data.replace(/SONG_COVER/g, song_name);
+          }
+
         data = data.replace(/SONG_NAME/g, song_name);
         data = data.replace(/CATEGORY/g, cate_name);
-        var song_lyric = fs.readFileSync('data/' + cate_name + '/' + song_name + '.txt', 'UTF-8');
+        var song_lyric;
+        try{
+          song_lyric = fs.readFileSync('data/songs/' + cate_name + '/' + song_name + '.txt', 'UTF-8');
+        }
+        catch(err){
+          song_lyric = fs.readFileSync('data/meta/lyrics.txt', 'UTF-8');
+        }
         data = data.replace(/SONG_LYRIC/g, song_lyric);
         response.write(data);
         response.end();
@@ -114,17 +186,14 @@ function start(route) {
       else if(pathname == PREFIX + '/random.html'){
         pathname = PREFIX + '/song.html';
         // get file list
-        var catid = Math.floor(Math.random()*2+1);
+        var cates = fs.readdirSync(PREFIX + '/data/songs/');
+
+        var catid = Math.floor(Math.random()*cates.length);
 
         var cate_name = '';
-        var cate_folder = "";
-        if(catid == 1){
-          cate_name = '金木水火土';
-        }
-        else if(catid ==2){
-          cate_name = '遇见一个人的星光';
-        }
-        cate_folder = PREFIX + '/data/' + cate_name + '/';
+        var cate_folder = '';
+        cate_name = cates[catid];
+        cate_folder = PREFIX + '/data/songs/' + cate_name + '/';
         files = fs.readdirSync(cate_folder);
         var songs = [];
         for (var i = 0; i < files.length; ++i) {
@@ -140,26 +209,55 @@ function start(route) {
           }
         }
 
-        var song_id = Math.floor(Math.random()*(songs.length-2)+2);
+        var song_id = Math.floor(Math.random()*songs.length);
         var song_name = songs[song_id];
         response.writeHead(200, {
           "Content-Type": "text/html"
         });
         pathname = decodeURIComponent(pathname);
         var data = fs.readFileSync(pathname, 'UTF-8');
+
+        var i;
+        for(i = 0; i < files.length; ++i){
+          if(files[i] == songs[song_id] + '.png'){
+            break;
+          }
+        }
+        if(i == files.length){
+          data = data.replace(/songs\/CATEGORY\/SONG_COVER/g, 'meta/cover');
+        }
+        else{
+          data = data.replace(/SONG_COVER/g, song_name);
+        }
+
         data = data.replace(/SONG_NAME/g, song_name);
         data = data.replace(/CATEGORY/g, cate_name);
-        var song_lyric = fs.readFileSync('data/' + cate_name + '/' + song_name + '.txt', 'UTF-8');
+        var song_lyric;
+        try{
+          song_lyric = fs.readFileSync('data/songs/' + cate_name + '/' + song_name + '.txt', 'UTF-8');
+        }
+        catch(err){
+          song_lyric = fs.readFileSync('data/meta/lyrics.txt', 'UTF-8');
+        }
         data = data.replace(/SONG_LYRIC/g, song_lyric);
         response.write(data);
         response.end();
       }
+      else if(pathname == PREFIX + '/about.html'){
+        // about page
+        pathname = decodeURIComponent(pathname);
+        fs.readFile(pathname, 'UTF-8', function(err, data) {
+          if (err) {
+            console.log(err);
+          } else {
+            response.write(data);
+            response.end();
+          }
+        })
+      }
       else{
         // other html files
-        response.writeHead(200, {
-        "Content-Type": "text/html"
-        });
-        pathname = decodeURIComponent(pathname);
+        pathname = 'error404.html';
         fs.readFile(pathname, 'UTF-8', function(err, data) {
           if (err) {
             console.log(err);
@@ -231,6 +329,34 @@ function start(route) {
     else if (suffix == "png") {
       response.writeHead(200, {
         "Content-Type": "image/png"
+      });
+      pathname = decodeURIComponent(pathname);
+      fs.readFile(pathname, function(err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          response.write(data);
+          response.end();
+        }
+      })
+    }
+    else if(suffix == "svg"){
+      response.writeHead(200, {
+        "Content-Type": "text/xml"
+      });
+      pathname = decodeURIComponent(pathname);
+      fs.readFile(pathname, function(err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          response.write(data);
+          response.end();
+        }
+      })
+    }
+    else{
+      response.writeHead(200, {
+        "Content-Type": "application/octet-stream"
       });
       pathname = decodeURIComponent(pathname);
       fs.readFile(pathname, function(err, data) {
